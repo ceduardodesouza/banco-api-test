b5b41e8ec0bc01338d07842c2d334bd71637becb
Carlos Eduardo de Souza <c.eduardosouza.prof@gmail.com>
2026-04-04 21:23:36 -0300

    Adicionando dotenv para variáveis de ambiente

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,11 @@
 const request = require ('supertest');
 const{expect} = require ('chai')
+require ('dotenv').config()
 
 describe('login', () => {
      describe ('POST /login', () => {
         it('Deve retornar 200 com token em string quando usar credenciais validas', async ()=>{
-            const resposta =  await request ('http://localhost:3000')
+            const resposta =  await request (process.env.BASE_URL)
                 .post('/login')   
                 .set('content-type' , 'application/json')
                  .send ({
